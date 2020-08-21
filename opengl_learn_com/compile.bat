61630d5217f80393fb49619b28ec68445791b043
BlueWorldInc <keamybakunin@gmail.com>
2020-08-21 18:18:27 +0200

    [ref] refactored the hierachy

---
+++ opengl_learn_com/compile.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Compiling...
+g++ -L lib -I include .\main_opengl_learn_com.cpp .\glad.c -o app -lglew32 -lopengl32 -lglu32 -lglfw3 -lgdi32 && powershell write-host -fore Green Compile success && .\app.exe
